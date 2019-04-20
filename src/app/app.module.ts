a90da9292f51ab03f060cfc27260d3b056defb15
Gorchak_UA <gorchakua@gmail.com>
2019-04-20 10:54:37 +0300

    upload project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbartopComponent } from './navbartop/navbartop.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbartopComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
